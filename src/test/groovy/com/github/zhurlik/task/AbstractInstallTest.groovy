be3453c08e1084dd9406349918423d50aa52d593
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-02-14 13:31:04 +0300

    more unit tests

--- src/test/groovy/com/github/zhurlik/task/AbstractInstallTest.groovy
+++ src/test/groovy/com/github/zhurlik/task/AbstractInstallTest.groovy
@@ -3,10 +3,11 @@ package com.github.zhurlik.task
 import org.gradle.api.Project
 import org.gradle.internal.impldep.org.codehaus.plexus.interpolation.os.Os
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
 import org.junit.Test
 
 import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
 
 /**
@@ -15,13 +16,35 @@ import static org.junit.Assert.assertTrue
  * @author zhurlik@gmail.com
  */
 class AbstractInstallTest {
-    @Test
-    void testIsLinux() {
+    private TestTask task
+
+    @Before
+    void setUp() throws Exception {
         final Project project = ProjectBuilder.builder().build()
         project.tasks.create('testTask', TestTask)
 
-        final TestTask task = project.tasks['testTask']
+        task = project.tasks['testTask']
+        assertNotNull(task)
+    }
+
+    @Test
+    void testIsLinux() {
         assertEquals(Os.isFamily(Os.FAMILY_UNIX), task.isLinux())
+    }
+
+    @Test
+    void testIsWindows() {
+        assertEquals(Os.isFamily(Os.FAMILY_WINDOWS), task.isWindows())
+    }
+
+    @Test
+    void testArch() {
+        assertTrue(task.is64())
+    }
+
+    @Test
+    void testInfo() {
+        task.info()
     }
 
     private static class TestTask extends AbstractInstall{}

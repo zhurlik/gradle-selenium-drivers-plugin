9749885a49111070855a5c66062c90843178520b
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-02-14 12:18:31 +0300

    a list of browsers

---
+++ src/main/groovy/com/github/zhurlik/domain/Browsers.groovy
@@ -0,0 +1,19 @@
+package com.github.zhurlik.domain
+
+/**
+ * A list of supported browsers.
+ *
+ * @author zhurlik@gmail.com
+ */
+enum Browsers {
+    CHROME,
+    EDGE,
+    IE,
+    FIREFOX,
+    OPERA,
+    SAFARI,
+    REMOTE,
+    EVENT_FIRING,
+    HTML_UNIT,
+    PHANTOMJS
+}

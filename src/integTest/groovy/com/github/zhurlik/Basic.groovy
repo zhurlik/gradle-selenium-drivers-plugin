c7181fa3b98806c4242a4427aa321d19b48d774a
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-02-21 18:24:06 +0300

    to make snapshots

--- src/integTest/groovy/com/github/zhurlik/Basic.groovy
+++ src/integTest/groovy/com/github/zhurlik/Basic.groovy
@@ -2,6 +2,9 @@ package com.github.zhurlik
 
 import org.gradle.api.Task
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 abstract class Basic {
     /**
      * To be able to execute a gradle task.
@@ -14,5 +17,21 @@ abstract class Basic {
         }
 
         task.execute()
+    }
+
+    /**
+     * Makes screenshots.
+     *
+     * @param task Gradle task to get temporary folder
+     * @param screenshot bytes[]
+     * @param pageName a file will be as: temporary folder/{name}.png
+     */
+    protected void screenshot(final Task task, final byte[] screenshot, final String pageName ) {
+
+        final Path path = Paths.get("${task.temporaryDir}", "${pageName}.png")
+
+        new BufferedOutputStream(new FileOutputStream(path.toFile())).withCloseable {out->
+            out.write(screenshot)
+        }
     }
 }

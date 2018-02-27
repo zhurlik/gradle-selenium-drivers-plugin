cfa28f62f07c893cd0c5fe0e1116e8ab83969e2c
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-02-27 11:35:39 +0300

    phantomjs on mac os x

--- src/main/groovy/com/github/zhurlik/task/InstallPhantomJs.groovy
+++ src/main/groovy/com/github/zhurlik/task/InstallPhantomJs.groovy
@@ -28,28 +28,7 @@ class InstallPhantomJs extends AbstractInstall {
                  *  2. https://code.google.com/archive/p/phantomjs/downloads
                  */
                 {
-                    String url = null
-                    // first attempt on https://bitbucket.org/ariya/phantomjs/downloads/
-                    try {
-                        url = getUrlOnBitbucket()
-                        ant.get(src: url,
-                                dest: temporaryDir.path,
-                                skipexisting: true,
-                                verbose: true
-                        )
-                        logger.debug('Loaded from https://bitbucket.org/ariya/phantomjs/downloads/')
-                    } catch (BuildException ex) {
-                        // second attempt on https://code.google.com/archive/p/phantomjs/downloads
-                        url = getUrlOnGoogleCode()
-                        ant.get(src: url,
-                                dest: temporaryDir.path,
-                                skipexisting: true,
-                                verbose: true
-                        )
-                        logger.debug('Loaded from https://code.google.com/archive/p/phantomjs/downloads')
-                    }
-
-                    final String filename = Paths.get(new URI(url).getPath()).getFileName().toString()
+                    final String filename = downloadInstaller()
                     final String archive = "${temporaryDir.path}/$filename"
                     logger.debug("Downloaded: $archive")
                     final String target = "${project.buildDir}/browser/$browser/$browserVersion"
@@ -77,7 +56,53 @@ class InstallPhantomJs extends AbstractInstall {
                 {}
         )
 
-        macOsInstaller = new Installer({}, {})
+        macOsInstaller = new Installer(
+                {
+                final String filename = downloadInstaller()
+                final String archive = "${temporaryDir.path}/$filename"
+                logger.debug("Downloaded: $archive")
+                final String target = "${project.buildDir}/browser/$browser/$browserVersion"
+                project.copy {
+                    from project.tarTree(project.resources.zip(archive))
+                    into target
+                }
+                logger.quiet("$browser has been installed")
+                logger.debug("Installed to: $target")
+
+                System.properties['phantomjs.binary.path'] = Paths.get(target,
+                        "phantomjs-$browserVersion-macosx", 'bin', 'phantomjs').toString()
+                }, {}
+        )
+    }
+
+    /**
+     * Downloads and stores an installer in the temoprary folder.
+     *
+     * @return file name
+     */
+    String downloadInstaller() {
+        String url = null
+        // first attempt on https://bitbucket.org/ariya/phantomjs/downloads/
+        try {
+            url = getUrlOnBitbucket()
+            ant.get(src: url,
+                    dest: temporaryDir.path,
+                    skipexisting: true,
+                    verbose: true
+            )
+            logger.debug('Loaded from https://bitbucket.org/ariya/phantomjs/downloads/')
+        } catch (BuildException ex) {
+            // second attempt on https://code.google.com/archive/p/phantomjs/downloads
+            url = getUrlOnGoogleCode()
+            ant.get(src: url,
+                    dest: temporaryDir.path,
+                    skipexisting: true,
+                    verbose: true
+            )
+            logger.debug('Loaded from https://code.google.com/archive/p/phantomjs/downloads')
+        }
+
+        return Paths.get(new URI(url).getPath()).getFileName().toString()
     }
 
     /**
@@ -85,10 +110,15 @@ class InstallPhantomJs extends AbstractInstall {
      * For example:
      *      https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
      *      https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+     *      https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip
      *
      * @return url
      */
     String getUrlOnBitbucket() {
+        if (isMacOsX()) {
+            return "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$browserVersion-macosx.zip"
+        }
+
         final String platform = "${is64() ? 'linux-x86_64' : 'linux-i686'}"
         return "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$browserVersion-${platform}.tar.bz2"
     }
@@ -98,10 +128,16 @@ class InstallPhantomJs extends AbstractInstall {
      * For example:
      *      https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-1.9.2-linux-x86_64.tar.bz2
      *      https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-1.9.2-linux-i686.tar.bz2
+     *      https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-1.9.2-macosx.zip
      *
      * @return url
      */
     String getUrlOnGoogleCode() {
+        if (isMacOsX()) {
+            return "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-" +
+                    "$browserVersion-macosx.zip"
+        }
+
         final String platform = "${is64() ? 'linux-x86_64' : 'linux-i686'}"
         return "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-" +
                 "$browserVersion-${platform}.tar.bz2"

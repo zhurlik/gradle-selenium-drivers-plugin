f307cc07f787701ab8e737977bcdb04fae8ee944
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-03-03 17:46:09 +0300

    logic for extracting a browser from deb package

--- src/main/groovy/com/github/zhurlik/task/AbstractInstall.groovy
+++ src/main/groovy/com/github/zhurlik/task/AbstractInstall.groovy
@@ -204,6 +204,55 @@ abstract class AbstractInstall extends DefaultTask {
         }
     }
 
+    /**
+     * Extracts a deb files using ar, tar commands and copies to target.
+     *
+     * @param archive a deb file
+     * @param innerPath what should be copied
+     * @param target where will be extracted
+     */
+    protected void extractDeb(final String archive, final String innerPath, final String target) {
+        new ByteArrayOutputStream().withCloseable { out ->
+            // extract deb
+            ExecResult res = project.exec {
+                workingDir temporaryDir.path
+                commandLine 'ar', '-x', archive
+                standardOutput = out
+                ignoreExitValue = true
+            }
+
+            String log = out.toString()
+            if (res.exitValue == 0) { // success
+                logger.quiet("DEB file has been extracted")
+                logger.debug("Log: $log")
+            } else {
+                logger.error("A problem with extarcting deb file: $log")
+                res.rethrowFailure()
+            }
+
+            project.exec {
+                workingDir temporaryDir.path
+                commandLine 'tar', '-xf', 'data.tar.xz'
+                standardOutput = out
+                ignoreExitValue = true
+            }
+
+            log = out.toString()
+            if (res.exitValue == 0) { // success
+                logger.quiet("data.tar.xz file has been extracted")
+                logger.debug("Log: $log")
+            } else {
+                logger.error("A problem with extarcting data.tar.xz file: $log")
+                res.rethrowFailure()
+            }
+
+            project.copy {
+                from "${temporaryDir.path}/$innerPath"
+                into target
+            }
+        }
+    }
+
     protected void install() {
         info()
         onWindows()
